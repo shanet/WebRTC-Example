4b3e724988e495c57c110a50673e73692fc819a6
shane tully <esc201@gmail.com>
2015-03-08 12:56:08 -0700

    remove trailing spaces

--- client/webrtc.js
+++ client/webrtc.js
@@ -2,7 +2,7 @@ var localVideo;
 var remoteVideo;
 var peerConnection;
 var peerConnectionConfig = {'iceServers': [{'url': 'stun:stun.services.mozilla.com'}, {'url': 'stun:stun.l.google.com:19302'}]};
- 
+
 navigator.getUserMedia = navigator.getUserMedia || navigator.mozGetUserMedia || navigator.webkitGetUserMedia;
 window.RTCPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
 window.RTCIceCandidate = window.RTCIceCandidate || window.mozRTCIceCandidate || window.webkitRTCIceCandidate;
@@ -17,7 +17,7 @@ function pageReady() {
 
     var constraints = {
         video: true,
-        audio: true, 
+        audio: true,
     };
 
     if(navigator.getUserMedia) {

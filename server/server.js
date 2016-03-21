99ce55ceaf4ffeb766b52dd57b018e08a887a3d0
Sergey Aksenov <sergeax@gmail.com>
2016-03-21 13:49:35 +0300

    Add support for secured websocket (mandatory in Chrome since version 47)

--- server/server.js
+++ server/server.js
@@ -1,6 +1,26 @@
+var ws_cfg = {
+    ssl: true,
+    port: 3434,
+    ssl_key: '/path/to/your/ssl.key',
+    ssl_cert: '/path/to/your/ssl.bundle.crt'
+};
+
+var processRequest = function(req, res) {
+    console.log("Request received.")
+};
+
+var httpServ = require('https');
+var fs = require('fs');
+var app = null;
+
+app = httpServ.createServer({
+  key: fs.readFileSync(ws_cfg.ssl_key),
+  cert: fs.readFileSync(ws_cfg.ssl_cert)
+}, processRequest).listen(ws_cfg.port);
+
 var WebSocketServer = require('ws').Server;
 
-var wss = new WebSocketServer({port: 3434});
+var wss = new WebSocketServer({server: app});
 
 wss.broadcast = function(data) {
     for(var i in this.clients) {

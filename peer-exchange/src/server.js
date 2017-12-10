44b411880d95956b946285e0aca4397b135eef38
Pontus Alexander <pontus.alexander@gmail.com>
2017-12-10 18:42:08 +0100

    Server bootstrap.

--- peer-exchange/src/server.js
+++ peer-exchange/src/server.js
@@ -1,71 +1,10 @@
 const WebSocketServer = require('ws').Server;
-const Session = require('./models/session');
-const Client = require('./models/client');
-const { createId } = require('./random');
+const {Registry} = require('./models/registry.js');
+const {createConnectionHandler} = require('./handler.js');
 
 const PORT = process.env.PORT || 9000;
-
 const server = new WebSocketServer({port: 9000});
 
-const sessions = new Map;
-
-function createClient(conn, id = createId()) {
-    return new Client(conn, id);
-}
-
-function createSession(id = createId()) {
-    if (sessions.has(id)) {
-        throw new Error(`Session ${id} already exists`);
-    }
-
-    const session = new Session(id);
-    console.log('Creating session', session);
-
-    sessions.set(id, session);
-
-    return session;
-}
-
-function getSession(id) {
-    return sessions.get(id);
-}
-
-server.on('connection', conn => {
-    console.log('Connection established');
-    const client = createClient(conn);
-
-    conn.on('message', msg => {
-        console.log('Message received', msg);
-        const data = JSON.parse(msg);
-
-        if (data.type === 'create-session') {
-            const session = createSession();
-            session.join(client);
-
-            client.send({
-                type: 'session-created',
-                id: session.id,
-            });
-        } else if (data.type === 'join-session') {
-            const session = getSession(data.id) || createSession(data.id);
-            session.join(client);
-        } else {
-            client.broadcast(data);
-        }
-    });
-
-    conn.on('close', () => {
-        console.log('Connection closed');
-        const session = client.session;
-        if (session) {
-            session.leave(client);
-            if (session.clients.size === 0) {
-                sessions.delete(session.id);
-            }
-        }
-
-        console.log(sessions);
-    });
-});
+server.on('connection', createConnectionHandler(new Registry()));
 
 console.log(`Running on port ${PORT}`);
